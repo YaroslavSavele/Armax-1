f4e0956358f196f6402e38eec3c401ad3c36d380
YaroslavSavele <asis174@mail.ru>
2022-06-25 14:11:53 +0500

    added main page and page solution.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
 
 })
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.side-menu');
+const btnClose = document.querySelector('.side-menu__close');
+
+burger.addEventListener('click', () => {
+    menu.classList.remove('side-menu--close');
+});
+
+btnClose.addEventListener('click', (e) => {
+    e.preventDefault();
+    menu.classList.add('side-menu--close');
+});
